46daf50c04d0f4755b8e0446ce151fcfa6a96beb
reftch <j.developer@hotmail.com>
2026-06-26 06:33:24 +0200

    Updated project

--- src/main.cc
+++ src/main.cc
@@ -1,36 +1,13 @@
-#define CLIENT_TIMEOUT_SECONDS 1000
-
-#include <chrono>
-#include <iomanip>
-#include <sstream>
-
 #include "server.h"
 
 int main() {
-    static auto& log = http::Logger::getInstance();
-    http::Server s("0.0.0.0", 8080);
+    http::Server s;
 
-    // Register signal handler with capture
-    static auto s_ptr = &s;
-    signal(SIGINT, [](int) {
-        s_ptr->Stop();
+    s.setRoute<http::HttpMethod::GET>("/", [](const http::Request&, http::Response& res) {
+        res.setContent<http::ContentType::PLAIN_TEXT>("Hello, world!");
     });
 
-    s.SetRoute<http::HttpMethod::GET>("/", [](const http::Request&, http::Response& res) {
-        res.SetContent<http::ContentType::HTML>("index.html");
-    });
-
-    s.SetRoute<http::HttpMethod::GET>("/home", [](const http::Request& req, http::Response& res) {
-        log.Info("Request path: {}", req.path());
-        res.SetContent<http::ContentType::HTML>("home.html");
-    });
-
-    s.SetRoute<http::HttpMethod::GET>("/api/v1/inc/:v", [](const http::Request& req, http::Response& res) {
-        std::string value = req.params().at("v");
-        res.SetContent<http::ContentType::JSON>("{\"value\":\"" + std::to_string(std::stoi(value) + 1) + "\"}");
-    });
-
-    s.Start();
+    s.start();
 
     return 0;
 }
